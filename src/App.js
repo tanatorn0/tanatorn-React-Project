758fd338ea067df643e6fceed35d7358bfa7860b
tanatorn <tanatorn.nge@rmutto.ac.th>
2024-09-10 11:50:36 +0700

    ssadaw22

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -8,8 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/login' element={<Login/>} />
-        <Route exact path='/register' element={<Register/>}/>
+        <Route exact path='/signin' element={<SignIn/>} />
+        <Route exact path='/signup' element={<SignUp/>}/>
       </Routes>
     </Router>
   );
